a5e2e950f2ecbb4d8372dd52f6cea8ae43ec4c7f
Zephiris <ryozu@n1nj4.org>
2024-12-09 22:31:52 -0500

    Preview image

--- KiriaDLCPlugin.cs
+++ KiriaDLCPlugin.cs
@@ -13,7 +13,7 @@ namespace Mod_KiriaDLC;
 [BepInPlugin("net.ryozu.kiriadlc", "Kiria DLC", "1.0.0.0")]
 public class KiriaDLCPlugin : BaseUnityPlugin
 {
-    public static readonly bool DEBUG_MODE = true;
+    public static readonly bool DEBUG_MODE = false;
     public static readonly bool DEBUG_OVERRIDE = false;
     public static readonly int NUM_FLOORS = DEBUG_MODE ? 3 : 6;
     
