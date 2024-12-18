505f0ec3c003a43475db131b255f958376ef340d
Zephiris <ryozu@n1nj4.org>
2024-12-18 16:48:44 -0500

    Make sure on reset that player has map replace quest=

--- KIriaConsole.cs
+++ KIriaConsole.cs
@@ -9,6 +9,11 @@ public class KIriaConsole
     {
         Mod_KiriaDLC.QuestKiria quest = EClass.game.quests.Get<Mod_KiriaDLC.QuestKiria>();
         if (quest != null) quest.phase = 1;
+        Quest mapQuest = EClass.game.quests.GetGlobal("kiria_map_replace");
+        if (mapQuest is null)
+        {
+            EClass.game.quests.globalList.Add(Quest.Create("kiria_map_replace").SetClient(quest?.person.chara, false));
+        }
         return "Reset Kiria Quest to first stage";
     }
 }
